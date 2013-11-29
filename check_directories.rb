0812721af85ef7e3954b5f09f8e8ddae68f9d277
apatosin <apatosin@yandex-team.ru>
2013-11-29 19:58:17 +0400

    initial commit for the script

---
+++ check_directories.rb
@@ -0,0 +1,66 @@
+require 'digest/md5'
+
+# !!! Update pathes before launch
+path_1 = "from_w"
+path_2 = "from_s"
+
+def findUniqueHashesForDirectory(path)
+	puts "Looking into "+path+" directory.."	
+	
+	hashes = Hash.new(0)
+	
+		Dir.foreach(path) do |fname|
+			if fname == "." || fname == ".DS_Store" || fname == ".."
+				next 
+			end	
+			
+			hash = Digest::MD5.hexdigest(File.read(path+"/"+fname));
+			
+			if hashes.key?(hash)
+				puts "	Warning! '"+fname+"' is duplicate to '"+hashes[hash]+"'"
+			end
+		
+# 			puts fname.to_s+" "+hash
+			hashes[hash] = fname.to_s;
+	
+		end
+		
+	puts "Have found ("+hashes.count.to_s+") unique files"	
+	
+	return hashes;
+end
+
+def compareHashes(hashes_1, hashes_2, path_1, path_2)
+	puts "Comparing "+path_1+" with "+path_2
+	hashes_1.each do |hash_1, filename_1| 
+		found = false;
+		hashes_2.each do |hash_2, filename_2|
+			if hash_1 == hash_2
+				found = true;		
+			end
+		end 
+		
+		if found == false
+			puts "	"+filename_1+" not found in "+path_1;
+		end
+		
+	end
+	puts "Comparing completed!"
+end
+
+##########################################################################################
+
+if path_1 == path_2
+	puts "Cannot compare directory "+ path_1 +" with itself. Sorry =/"
+	abort
+end
+
+hashes_1 = findUniqueHashesForDirectory(path_1)
+puts "\n"
+hashes_2 = findUniqueHashesForDirectory(path_2)
+
+puts "\n"
+
+compareHashes(hashes_1, hashes_2, path_1, path_2)
+puts "\n"
+compareHashes(hashes_2, hashes_1, path_2, path_1)
